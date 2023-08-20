353200f319c54e54b966a1525a55216a50d14b8a
Jjaynxne <142704845+Jjaynxne@users.noreply.github.com>
2023-08-20 18:48:33 +0700

    first commit

---
+++ Algorhythm/Algorhythm/Form1.cs
@@ -0,0 +1,10 @@
+namespace Algorhythm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSHAP
+# CSHAP
