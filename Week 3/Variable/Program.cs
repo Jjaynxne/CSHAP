e537f72c6c49f8648aaa43297ba39fb954e09270
Jjaynxne <142704845+Jjaynxne@users.noreply.github.com>
2023-08-27 17:25:02 +0700

    Add files via upload

---
+++ Week 3/Variable/Program.cs
@@ -0,0 +1,39 @@
+﻿/* *** string ***
+string prifix = "Mr." ;
+string fname = "Intat";
+string lname = "Kanalaksombat";
+
+Console.WriteLine(fname);
+Console.WriteLine(fname);
+
+Console.WriteLine(prifix+" "+fname+" "+lname);*/
+
+
+//const int myNum = 20;
+//myNum = 5;//(15+1+2+4)/4;
+
+//double newNum = 5.5D;
+//float fnumer = 5.5;
+
+//Console.WriteLine(myNum);
+
+
+    /*การประกาศ const ***/
+    /* const double myNum = 3.14;
+    myNum = 20;
+    Console.WriteLine(myNum); */
+
+
+    /** Multiple Variable **/
+
+    /*int input1 = 1;
+    int input2 = 2;
+    int input3 = 3;*/
+
+    int input1=1, input2=2;
+     double input3=3.14, input4=1.22;
+     float fl = 3.33F;
+     string str = "Mr.";
+        char ch = 'c' ;
+
+    Console.WriteLine(str+" "+ch+" "+fl+" "+input3+" "+input1);
---
+++ Week 3/user-input/Program.cs
@@ -0,0 +1,21 @@
+﻿/* string fname = "";
+double height = 0;
+double weight = 0.0D;
+double BMI = 0.0D;
+
+
+Console.Write("Input First name: ");   // ชื่อเล่น
+fname = Console.ReadLine();
+
+Console.Write("Input Height: ");        // ส่วนสูง
+height = double.Parse(Console.ReadLine());
+
+Console.Write("Input Weight: ");        //น้ำหนัก
+weight = double.Parse(Console.ReadLine());
+
+BMI = height/weight;
+BMI = Math.Pow(BMI, 2);
+
+
+Console.WriteLine("BMI is: "+BMI);
+*/
