55891443deb3fd8839d4a696e2ad7c0b77225da5
Jjaynxne <142704845+Jjaynxne@users.noreply.github.com>
2023-09-02 09:37:52 +0700

    Add files via upload

---
+++ Week 3/Homework/Program.cs
@@ -0,0 +1,37 @@
+﻿string prefix = "";
+string fname = "";
+string lname = "";
+int     ID = 0;
+double height = 0;
+double weight = 0.0D;
+double BMI = 0.0D;
+
+
+Console.Write("Input Prefix: ");   
+prefix = Console.ReadLine();
+
+Console.Write("Input First name: ");   
+fname = Console.ReadLine();
+
+Console.Write("Input Last name: ");  
+lname = Console.ReadLine();
+
+Console.Write("Input Student ID: ");   
+ID = Int32.Parse(Console.ReadLine());
+
+Console.Write("Input Height: ");        
+height = double.Parse(Console.ReadLine());
+
+Console.Write("Input Weight: ");        
+weight = double.Parse(Console.ReadLine());
+
+BMI = height/weight;
+BMI = Math.Pow(BMI, 2);
+
+Console.WriteLine("********************");
+Console.WriteLine("Student ID: "+ID);
+Console.WriteLine("Fullname is: "+prefix+fname+" "+lname);
+Console.WriteLine("Your Heigt is: "+height);
+Console.WriteLine("Your Weight is: "+weight);
+Console.WriteLine("BMI is: "+BMI);
+Console.WriteLine("********************");  
