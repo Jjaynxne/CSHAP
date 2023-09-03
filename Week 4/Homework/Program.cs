03b30168d79ce638cac17b063e755df0e4df08ee
Jjaynxne <142704845+Jjaynxne@users.noreply.github.com>
2023-09-03 18:06:29 +0700

    Add files via upload

---
+++ Week 4/Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+
+string[] std = {"663245001", "663245002", "663245003","663245004", 
+                "663245005","663245006","663245009","664245025"};
+
+string[] prefix = {"Mr.", "Mr.", "Mr.", "Mr."
+                ,"Mr.","Ms.","Mr.","Ms."};
+
+string[] fname = {"Kittiphong", "Tanakorn", "Pongsakon", "Watcharapong"
+                ,"Intat","Kannikar","Kitisak","Thanaporn"};
+
+string[] lname = {"Chaimon", "Suphaphut", "Kongnoi", "Maneengam"
+                ,"Kanalaksombat","Patposri","Srisuk","Hongngam"};
+
+
+int i = 0;
+int seq = 0;
+    
+    Console.WriteLine("663245005 Mr.Intat Kanalaksombat");
+
+while (i < 8) 
+{
+
+    
+    Console.WriteLine(std[i]+" "+prefix[i]+fname[i]+" "+lname[i]);
+    seq++;
+    i++;
+}
---
+++ Week 4/Program.cs
@@ -0,0 +1,153 @@
+﻿/* int plus1 = 2;
+int plus2 = 3;
+int total;
+
+total = (plus1 + plus2);
+Console.Write(total); */ // ค่าบวก
+
+
+/* int minus1 = 4;
+int minus2 = 2;
+int total = minus1-minus2;
+Console.Write(total); */    // ค่าลบ
+
+
+/* int modu1 = 8;
+int modu2 = 3;
+
+int total = modu1 % modu2;
+Console.Write(total); */    //ค่าหาเศษ
+
+
+/* int plus = 0;
+int total = 0;
+
+plus++;
+total = plus;
+
+Console.Write(total); */    //ค่าบวก 1
+
+
+/* int minus_down = 10;
+int total = 0;
+
+minus_down--;
+total = minus_down;
+
+Console.Write(total); */    //ค่าลบ 1
+
+
+
+/* int min = Math.Min(5,10);
+Console.WriteLine(min);       
+
+int max = Math.Max(5,10);
+Console.Write(max); */     // ค่าน้อยกับค่ามาก
+
+
+/* string hello = "Hello";
+string world = "World";
+Console.WriteLine(hello+" "+world); */  //แสดงผลลัพธ์
+
+
+/* bool val = false;
+bool check = false;
+Console.WriteLine(val==check); */       // ใช่หรือไม่
+
+
+ /*int input;
+
+Console.Write("Input Value: ");   
+input = Int32.Parse(Console.ReadLine());
+
+
+if (input == 2)
+{
+    Console.WriteLine("True Success");
+}
+
+else if (input == 3)
+{
+    Console.WriteLine("Yes this is 3 value Success");
+}
+
+else if (input == 4)
+{
+    Console.WriteLine("Yes this is 4 value Success");
+}
+
+else if (input == 5)
+{
+    Console.WriteLine("Yes this is 5 value Success");
+}
+
+else if(input == 6 || input == 12)
+{
+    Console.WriteLine("Yes this is 6 or 12 value Success");
+}
+
+else
+{
+    Console.WriteLine("False");                 // IF-Else
+} */
+
+
+
+/*int day = 2;
+
+switch (day) 
+{
+    case 1:
+    Console.WriteLine("Monday");
+    break;
+  
+    case 2:
+    Console.WriteLine("Tuesday");
+    break;
+  
+    case 3:
+    Console.WriteLine("Wednesday");
+    break;
+  
+    case 4:
+    Console.WriteLine("Thursday");
+    break;
+  
+    case 5:
+    Console.WriteLine("Friday");
+    break;
+  
+    case 6:
+    Console.WriteLine("Saturday");
+    break;
+
+    case 7:
+    Console.WriteLine("Sunday");
+    break;
+
+    default:
+    Console.WriteLine("Not True");
+    break;
+} */
+
+
+
+string[] cars = {"Volvo", "BMW", "Ford", "Mazda"
+                ,"Honda","Tesla","Nissan","Lambogini"};
+
+string[] color = {"Red", "Blue", "Black", "White"
+                ,"Green","Purple","Brown","Yellow"};
+
+
+int i = 0;
+int seq = 0;
+while (i < 8) 
+{
+
+    seq++;
+  Console.WriteLine(seq+"."+cars[i]+" "+"Color is "+color);
+  i++;
+}
+
+
+
